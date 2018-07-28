70f49ed10f1b3fe570e78d9afc551ee1940a584f
Alyssa Horrocks <alyssa.horrocks2@gmail.com>
2018-07-28 14:51:20 -0700

    update result component to require player names

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,14 +5,22 @@ import {connect} from 'react-redux';
 class Result extends Component {
   render () {
     let result = ''; 
-    if (this.props.turn) {
-      result = `It's ${this.props.turn.toUpperCase()}'s turn.`;
+    
+    if(this.props.players.O.name === '' || this.props.players.X.name === '') {
+      result = `Please enter player names to begin the game.`
+    } else {
+      if (this.props.turn) {
+        const turn = this.props.turn.toUpperCase();
+        result = `It's ${this.props.players[turn].name}'s turn.`;
+      }
+      if (this.props.won) {
+        const won = this.props.won.toUpperCase();
+        result = `Yay! ${this.props.players[won].name} won!`
+      } else if (this.props.draw) {
+        result = 'We have a draw!';
+      }
     }
-    if (this.props.won) {
-      result = `Yay! ${this.props.won.toUpperCase()} won!`
-    } else if (this.props.draw) {
-      result = 'We have a draw!';
-    }
+    
     return (
       <div>
         <p>
@@ -26,12 +34,18 @@ class Result extends Component {
 Result.propTypes = {
   won: PropTypes.string,
   turn: PropTypes.string.isRequired,
-  draw: PropTypes.bool.isRequired
+  draw: PropTypes.bool.isRequired,
+  players: PropTypes.shape({
+    X: PropTypes.shape({
+      name: PropTypes.string}), 
+    O: PropTypes.shape({
+      name: PropTypes.string})
+  }).isRequired
 };
 
 export default connect(
-  ({won, turn, draw}) => ({
-    won, turn, draw
+  ({won, turn, draw, players}) => ({
+    won, turn, draw, players
   })
 )(Result);
 
--- src/components/Result.test.js
+++ src/components/Result.test.js
@@ -2,17 +2,35 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { PureResult as Result } from './Result';
 
+const players = {
+  X: {
+    name: 'Jane'
+  },
+  O: {
+    name: 'John'
+  }
+};
+
 it('Should render the Result component with message about current turn', () => {
-  const wrapper = shallow(<Result turn={'o'} draw={false} />);
-  expect(wrapper.find('p').node.props.children).toEqual('It\'s O\'s turn.');
+  const wrapper = shallow(<Result turn={'o'} draw={false} players={players} />);
+  expect(wrapper.find('p').node.props.children).toEqual(`It\'s ${players.O.name}\'s turn.`);
 });
 
 it('Should render the Result component with message about winning symbol', () => {
-  const wrapper = shallow(<Result won={'x'} draw={false} turn={'x'} />);
-  expect(wrapper.find('p').node.props.children).toEqual('Yay! X won!');
+  const wrapper = shallow(<Result won={'x'} draw={false} turn={'x'} players={players} />);
+  expect(wrapper.find('p').node.props.children).toEqual(`Yay! ${players.X.name} won!`);
 });
 
 it('Should render the Result component with message about the draw', () => {
-  const wrapper = shallow(<Result draw={true} turn={'x'} />);
+  const wrapper = shallow(<Result draw={true} turn={'x'} players={players} />);
   expect(wrapper.find('p').node.props.children).toEqual('We have a draw!');
+});
+
+it('Should render the Result component with message about needing player names to begin', () => {
+  const noPlayerNames = {
+    X: { name: '' }, 
+    O: { name: '' }
+  }
+  const wrapper = shallow(<Result draw={true} turn={'x'} players={noPlayerNames} />);
+  expect(wrapper.find('p').node.props.children).toEqual(`Please enter player names to begin the game.`);
 });
